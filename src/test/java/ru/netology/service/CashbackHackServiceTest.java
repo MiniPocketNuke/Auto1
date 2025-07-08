481d40ae71e91f3fd624f037aac90a2540176c8d
Mah <timohaarsentiev1@gmail.com>
2025-07-09 01:46:04 +0300

    TestNG

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,32 +1,32 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
-import org.testng.Assert;
+import org.junit.Test;
+import org.junit.Assert;
 public class CashbackHackServiceTest {
 
 
     @Test
-    void CashbackTest(){
+    public void CashbackTest(){
     CashbackHackService service = new CashbackHackService();
     int amount = 900;
     int expected = 100;
     int actual = service.remain(amount);
-    Assert.assertEquals(actual, expected);}
+    Assert.assertEquals(expected, actual);}
 
     @Test
-    void CashbackTest2(){
+    public void CashbackTest2(){
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int expected = 0;
         int actual = service.remain(amount);
-        Assert.assertEquals(actual, expected);}
+        Assert.assertEquals(expected, actual);}
     @Test
-    void CashbackTest3(){
+    public void CashbackTest3(){
         CashbackHackService service = new CashbackHackService();
         int amount = 1;
         int expected = 999;
         int actual = service.remain(amount);
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
 
 
 }}
